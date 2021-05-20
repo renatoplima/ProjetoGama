f470d35058ef8da42d84b4f0e0f69c4fafdf01bd
Raphael Gomes <raphgom@gmail.com>
2021-05-20 12:14:08 -0300

    Atualização AgenteFinanceiro

--- itau/src/main/java/br/com/itau/controller/AgenteFinanceiroController.java
+++ itau/src/main/java/br/com/itau/controller/AgenteFinanceiroController.java
@@ -1,5 +1,30 @@
 package br.com.itau.controller;
 
-public class AgenteFinanceiroController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.itau.beans.AgenteFinanceiro;
+import br.com.itau.dao.AgenteFinanceiroDAO;
+
+@RestController
+@CrossOrigin("*")
+public class AgenteFinanceiroController {
+	
+	@Autowired
+	private AgenteFinanceiroDAO dao;
+	
+	@GetMapping("/topagentesfinanceiros")
+	public ResponseEntity<List<AgenteFinanceiro>> getTopAgentesFinanceiros(){
+		List<AgenteFinanceiro> resposta = dao.findByTopTen();
+		if(resposta==null) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(resposta);
+	}
+	
 }
