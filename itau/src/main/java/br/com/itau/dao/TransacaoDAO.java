cd126a4d78a8570290921190a96a103f9ee06516
renatoplima <83669884+renatoplima@users.noreply.github.com>
2021-05-20 16:29:02 -0300

    Update TransacaoDAO.java
    fix

--- itau/src/main/java/br/com/itau/dao/TransacaoDAO.java
+++ itau/src/main/java/br/com/itau/dao/TransacaoDAO.java
@@ -8,7 +8,7 @@ import br.com.itau.beans.Transacao;
 
 public interface TransacaoDAO extends CrudRepository <Transacao, Integer>{
 
-	@Query(value="select count(id_transacao) as Total from mtb310_transaction inner join mtb310_ag_financeiro on mtb310_ag_financeiro.id_agente = mtb310_transaction.ag_financeiro_id_agente where mtb310_ag_financeiro.id_agente=:codagente and mtb310_transaction.status=:codstatus",
-			nativeQuery=true)
-	public int findByTotalTransacoesAgenteEStatus(@Param("codagente") int codagente, @Param("codstatus") int codstatus);
+        @Query(value="select count(id_transacao) as Total from mtb310_transaction inner join mtb310_ag_financeiro on mtb310_ag_financeiro.id_agente = mtb310_transaction.ag_financeiro_id_agente where mtb310_ag_financeiro.id_agente=:codagente and mtb310_transaction.valor_solic=:codstatus",
+                        nativeQuery=true)
+        public int findByTotalTransacoesAgenteEStatus(@Param("codagente") int codagente, @Param("codstatus") int codstatus);
 }
