713f0c60667f20d669692810afc7f460922fbea7
renatoplima <83669884+renatoplima@users.noreply.github.com>
2021-05-20 16:29:37 -0300

    Update AgenteFinanceiroDAO.java
    fix

--- itau/src/main/java/br/com/itau/dao/AgenteFinanceiroDAO.java
+++ itau/src/main/java/br/com/itau/dao/AgenteFinanceiroDAO.java
@@ -9,11 +9,11 @@ import br.com.itau.beans.AgenteFinanceiro;
 
 public interface AgenteFinanceiroDAO extends CrudRepository<AgenteFinanceiro, Integer> {
 
-	public List<AgenteFinanceiro> findAll();
-	
-	@Query(value="select id_agente, nome_agente, volume_transacional from MTB310_ag_financeiro GROUP BY nome_agente ORDER BY volume_transacional desc LIMIT 10", nativeQuery=true)
-	public List<AgenteFinanceiro> findByTopTen();
-	
-//	public List<AgenteFinanceiro> findByNomeAgenteFinanceiroLike(String agente);
-	
+        public List<AgenteFinanceiro> findAll();
+
+        @Query(value="select id_agente, nome_agente, volume_transacional from MTB310_ag_financeiro GROUP BY nome_agente ORDER BY volume_transacional desc LIMIT 10", nativeQuery=true)
+        public List<AgenteFinanceiro> findByTopTen();
+
+        public List<AgenteFinanceiro> findByNomeAgenteLike(String agente);
+
 }
