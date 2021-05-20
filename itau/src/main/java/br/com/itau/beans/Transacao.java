1e5a5d2c0d86baf71172968f033243920dabc8a1
Renato P. Lima <renatoplima@renatoplima.com.br>
2021-05-20 11:28:42 -0300

    Ajustando AgenteFinanceiro.java e Transacao.java

--- itau/src/main/java/br/com/itau/beans/Transacao.java
+++ itau/src/main/java/br/com/itau/beans/Transacao.java
@@ -2,7 +2,6 @@ package br.com.itau.beans;
 
 import java.util.Date;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -44,73 +43,90 @@ public class Transacao {
 	
 	@ManyToOne
 	@JsonIgnoreProperties("transacoes")
-	private int ag_financeiro;
+	private AgenteFinanceiro ag_financeiro;
 	
-	public int getId_transacao() {
-		return id_transacao;
-	}
-	public void setId_transacao(int id_transacao) {
-		this.id_transacao = id_transacao;
-	}
-	public Date getData_hora() {
-		return data_hora;
-	}
-	public void setData_hora(Date data_hora) {
-		this.data_hora = data_hora;
-	}
-	public int getDispositivo() {
-		return dispositivo;
-	}
-	public void setDispositivo(int dispositivo) {
-		this.dispositivo = dispositivo;
-	}
-	public float getValor_solic() {
-		return valor_solic;
-	}
-	public void setValor_solic(float valor_solic) {
-		this.valor_solic = valor_solic;
-	}
-	public float getValor_aut() {
-		return valor_aut;
-	}
-	public void setValor_aut(float valor_aut) {
-		this.valor_aut = valor_aut;
-	}
-	public int getStatus() {
-		return status;
-	}
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	public int getAg_financeiro() {
-		return ag_financeiro;
-	}
-	public void setAg_financeiro(int ag_financeiro) {
-		this.ag_financeiro = ag_financeiro;
-	}
-	
-	public Transacao(int id_transacao, Date data_hora, int dispositivo, float valor_solic, float valor_aut, int status,
-			int ag_financeiro) {
-		super();
-		this.id_transacao = id_transacao;
-		this.data_hora = data_hora;
-		this.dispositivo = dispositivo;
-		this.valor_solic = valor_solic;
-		this.valor_aut = valor_aut;
-		this.status = status;
-		this.ag_financeiro = ag_financeiro;
-	}
-	
-	public Transacao() {
-		super();
-	}
-	
+
 	@Override
 	public String toString() {
 		return "Transacao [id_transacao=" + id_transacao + ", data_hora=" + data_hora + ", dispositivo=" + dispositivo
 				+ ", valor_solic=" + valor_solic + ", valor_aut=" + valor_aut + ", status=" + status
 				+ ", ag_financeiro=" + ag_financeiro + "]";
 	}
-	
+
+	public Transacao(int id_transacao, Date data_hora, int dispositivo, float valor_solic, float valor_aut, int status,
+		AgenteFinanceiro ag_financeiro) {
+	super();
+	this.id_transacao = id_transacao;
+	this.data_hora = data_hora;
+	this.dispositivo = dispositivo;
+	this.valor_solic = valor_solic;
+	this.valor_aut = valor_aut;
+	this.status = status;
+	this.ag_financeiro = ag_financeiro;
+}
+
+	public Transacao() {
+	super();
+}
+
+	public int getId_transacao() {
+		return id_transacao;
+	}
+
+	public void setId_transacao(int id_transacao) {
+		this.id_transacao = id_transacao;
+	}
+
+	public Date getData_hora() {
+		return data_hora;
+	}
+
+	public void setData_hora(Date data_hora) {
+		this.data_hora = data_hora;
+	}
+
+	public int getDispositivo() {
+		return dispositivo;
+	}
+
+	public void setDispositivo(int dispositivo) {
+		this.dispositivo = dispositivo;
+	}
+
+	public float getValor_solic() {
+		return valor_solic;
+	}
+
+	public void setValor_solic(float valor_solic) {
+		this.valor_solic = valor_solic;
+	}
+
+	public float getValor_aut() {
+		return valor_aut;
+	}
+
+	public void setValor_aut(float valor_aut) {
+		this.valor_aut = valor_aut;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public AgenteFinanceiro getAg_financeiro() {
+		return ag_financeiro;
+	}
+
+	public void setAg_financeiro(AgenteFinanceiro ag_financeiro) {
+		this.ag_financeiro = ag_financeiro;
+	}
 	
 }
+	
+	
+	
+	
