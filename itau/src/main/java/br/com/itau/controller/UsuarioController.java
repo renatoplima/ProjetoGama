392b6ffd8bca67da181d7c4d6fc1d7550bcaf0bf
Raphael Gomes <raphgom@gmail.com>
2021-05-20 11:50:49 -0300

    Atualizacao Controllers

---
+++ itau/src/main/java/br/com/itau/controller/UsuarioController.java
@@ -0,0 +1,5 @@
+package br.com.itau.controller;
+
+public class UsuarioController {
+
+}
--- itau/src/main/java/br/com/itau/dao/UsuarioDAO.java
+++ itau/src/main/java/br/com/itau/dao/UsuarioDAO.java
@@ -1,6 +1,5 @@
 package br.com.itau.dao;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.itau.beans.Usuario;
