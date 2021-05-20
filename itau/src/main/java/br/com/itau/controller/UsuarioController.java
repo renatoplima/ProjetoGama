3a0f3ba7a1094021715f7770a62ce4d986cce3a2
Renato P. Lima <renatoplima@renatoplima.com.br>
2021-05-20 12:49:46 -0300

    ajustes classes usuario

--- itau/src/main/java/br/com/itau/controller/UsuarioController.java
+++ itau/src/main/java/br/com/itau/controller/UsuarioController.java
@@ -1,5 +1,68 @@
 package br.com.itau.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.itau.beans.Usuario;
+import br.com.itau.dao.UsuarioDAO;
+
+@RestController // para indicar ao Spring Boot que esta classe irá responder métodos do HTTP
+@CrossOrigin("*") // para liberar o acesso fora do servidor à estes recursos
 public class UsuarioController {
+	
+	@Autowired // delega para Spring Boot o gerenciamento deste atributo
+	private UsuarioDAO dao; 
+	
+	@PostMapping("/login") // Processamento esta no BackEnd pois solicito para o banco a lista de todos os usuário e na aplicação que realizo o filtro.
+	public ResponseEntity<Usuario> logar(@RequestBody Usuario objeto){
+		List<Usuario> lista = (List<Usuario>) dao.findAll();
+		for (Usuario usuario : lista) {
+			if (usuario.getRacf().equals(objeto.getRacf()) || usuario.getEmail().equals(objeto.getEmail()) && usuario.getSenha().equals(objeto.getSenha())) {
+				return ResponseEntity.ok(usuario);
+			}
+		}
+		return ResponseEntity.status(404).build();
+	}
+
+	
+	@GetMapping("/user/{cod}")
+	public ResponseEntity<Usuario> getUser(@PathVariable int cod) {
+		Usuario resposta = dao.findById(cod).orElse(new Usuario());
+		resposta.setSenha("****");
+		if (resposta.getId()==0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(resposta);
+	}
+
+
+	@PostMapping("/novousuario")
+	public ResponseEntity<Usuario> add(@RequestBody Usuario objeto) {
+		try {
+			dao.save(objeto);
+			return ResponseEntity.ok(objeto);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(404).build();
+		}
+
+	}
+		
+	@GetMapping("/users")
+	public ResponseEntity<List<Usuario>> getAll(){
+		List<Usuario> lista = (List<Usuario>)dao.findAll();
+		if (lista.size()==0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(lista);
+	}
 
 }
--- itau/src/main/java/br/com/itau/dao/UsuarioDAO.java
+++ itau/src/main/java/br/com/itau/dao/UsuarioDAO.java
@@ -1,12 +1,16 @@
 package br.com.itau.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.itau.beans.Usuario;
 
 public interface UsuarioDAO extends CrudRepository<Usuario, Integer>{
 	
-//	@Query(value="select * from tb_usuario where racf=:usuario or email:email and senha=:senha", nativeQuery=true)
-//	public int findByRacfAndEmailAndSenha(@Param("usuario","senha") String usuario, String senha);
+	@Query(value="select * from tb_usuario where racf=:racf or email:email and senha=:senha", nativeQuery=true)
+	public int findByRacfAndEmailAndSenha(@Param("racf")String racf, @Param("email")String email, @Param("senha")String senha);
+	
+	public Usuario findByEmailAndSenha(String email, String senha);
 	
 }
